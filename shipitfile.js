df82e0c09920e0baa8e3f20dad3f67485352b794
agik-setiawan <setiawanagik@gmail.com>
2023-08-20 21:52:36 +0700

    update env

--- shipitfile.js
+++ shipitfile.js
@@ -24,7 +24,7 @@ module.exports = shipit => {
         // await shipit.remote(`mkdir ~/${dir}`);
         // await shipit.remote(`cd ~/${dir} && git clone ${git} .`);
         await shipit.remote(`cd ~/${dir} && git pull ${git}`);
-        // await shipit.remote(`cd ~/${dir} && cp .env.prod .env`);
+        await shipit.remote(`cd ~/${dir} && cp .env.prod .env`);
         // await shipit.remote(`cd ~/${dir} && npm install`);
         await shipit.remote(`cd ~/${dir} && npm run build`);
         // await shipit.remote(`cd ~/${dir} && pm2 start yarn --name strapi -- start`);
