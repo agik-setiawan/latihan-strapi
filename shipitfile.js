ce914bac87d25475b926b3d63c2b095bf66a0ad7
agik-setiawan <setiawanagik@gmail.com>
2023-08-20 21:48:11 +0700

    update port to 80

--- shipitfile.js
+++ shipitfile.js
@@ -21,13 +21,14 @@ module.exports = shipit => {
         const git = "https://github.com/agik-setiawan/latihan-strapi.git";
         const dir = "strapi";
 
-        await shipit.remote(`mkdir ~/${dir}`);
-        await shipit.remote(`cd ~/${dir} && git clone ${git} .`);
+        // await shipit.remote(`mkdir ~/${dir}`);
+        // await shipit.remote(`cd ~/${dir} && git clone ${git} .`);
         await shipit.remote(`cd ~/${dir} && git pull ${git}`);
-        await shipit.remote(`cd ~/${dir} && cp .env.prod .env`);
-        await shipit.remote(`cd ~/${dir} && npm install`);
+        // await shipit.remote(`cd ~/${dir} && cp .env.prod .env`);
+        // await shipit.remote(`cd ~/${dir} && npm install`);
         await shipit.remote(`cd ~/${dir} && npm run build`);
-        await shipit.remote(`cd ~/${dir} && pm2 start yarn --name strapi -- start`);
+        // await shipit.remote(`cd ~/${dir} && pm2 start yarn --name strapi -- start`);
+        await shipit.remote(`cd ~/${dir} && pm2 restart strapi`);
     });
 
 };
